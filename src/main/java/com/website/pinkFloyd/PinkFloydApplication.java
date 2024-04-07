dc430cf26b5b096aa8c72da2dff04e2540e80f94
Dmitriy-684 <dmitriyegorov111@mail.ru>
2024-04-07 11:36:08 +0300

    init Spring project

---
+++ src/main/java/com/website/pinkFloyd/PinkFloydApplication.java
@@ -0,0 +1,13 @@
+package com.website.pinkFloyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkFloydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinkFloydApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/website/pinkFloyd/PinkFloydApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.pinkFloyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinkFloydApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
